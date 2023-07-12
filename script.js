9d187dfc298bed007c7ee797818f1789a430c413
sanj222 <134222801+sanj222@users.noreply.github.com>
2023-07-12 22:50:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
-//your JS code here. If required.
+function findFirstNonRepeatingChar() {
+  const inputString = prompt("Enter a string:");
+
+  const charCount = {};
+
+
+  for (let char of inputString) {
+    if (charCount[char]) {
+      charCount[char]++;
+    } else {
+      charCount[char] = 1;
+    }
+  }
+
+  
+  for (let char of inputString) {
+    if (charCount[char] === 1) {
+      alert(`The first non-repeating character is: ${char}`);
+      return;
+    }
+  }
+
+  alert("No non-repeating character found.");
+}
+findFirstNonRepeatingChar();
+
+
